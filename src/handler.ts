6bd2170a550ea3ef57d9797583822671eff0b80a
wangpeng1 <wangpeng1@wisenergy.cn>
2022-04-03 17:51:01 +0800

    2

--- src/handler.ts
+++ src/handler.ts
@@ -1,18 +1,3 @@
-// export async function handleRequest(request: Request): Promise<Response> {
-//   return new Response(`request method: ${request.method}`)
-// }
-
-export async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  });
+export async function handleRequest(request: Request): Promise<Response> {
+  return new Response(`request method: ${request.method}`)
 }
-// async function handleRequest(request) {
-//   let response;
-//   if (request.method === 'POST') {
-//     response = await generate(request);
-//   } else {
-//     response = new Response('Expected POST', { status: 500 });
-//   }
-//   // ...
-// }
